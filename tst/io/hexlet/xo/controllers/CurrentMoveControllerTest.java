0f3d43fd97a78ee3f540296038f1b3b92b6af54c
MasterNomad <UnboundNomad@gmail.com>
2018-06-02 20:33:26 +0400

    CurrentMoveTest - initial

---
+++ tst/io/hexlet/xo/controllers/CurrentMoveControllerTest.java
@@ -0,0 +1,15 @@
+package io.hexlet.xo.controllers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CurrentMoveControllerTest {
+
+    @Test
+    public void currentMove() {
+
+
+
+    }
+}
