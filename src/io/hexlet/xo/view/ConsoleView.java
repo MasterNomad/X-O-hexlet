d2269d06be8cbd0fcea69646253d560a4d10890d
MasterNomad <UnboundNomad@gmail.com>
2018-06-04 11:53:51 +0400

    ConsoleView - update

--- src/io/hexlet/xo/view/ConsoleView.java
+++ src/io/hexlet/xo/view/ConsoleView.java
@@ -1,13 +1,23 @@
 package io.hexlet.xo.view;
 
+import io.hexlet.xo.controllers.CurrentMoveController;
+import io.hexlet.xo.controllers.MoveController;
+import io.hexlet.xo.controllers.WinnerController;
 import io.hexlet.xo.model.Field;
 import io.hexlet.xo.model.Figure;
 import io.hexlet.xo.model.Game;
 import io.hexlet.xo.model.exceptions.InvalidPointException;
+import io.hexlet.xo.model.exceptions.PointAlreadyOccupiedException;
 
 import java.awt.*;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class ConsoleView {
+
+    private final CurrentMoveController currentMoveController = new CurrentMoveController();
+    private final WinnerController winnerController = new WinnerController();
+    private final MoveController moveController = new MoveController();
 
     public void show(final Game game) {
 
@@ -16,19 +26,75 @@ public class ConsoleView {
         final Field field = game.getField();
 
         for (int x = 0; x < field.getSize(); x += 1) {
-            printeLine(field, x);
-            if (x != field.getSize() - 1) printSeparator();
+            if (x != 0) printSeparator();
+            printLine(field, x);
+        }
+        System.out.println();
+        System.out.println();
+
+    }
+
+
+    public boolean move(final Game game) {
+
+        final Field field = game.getField();
+        final Figure winner = winnerController.getWinner(field);
+
+        if (winner != null) {
+            System.out.format("Winner is %s! Congratulations!\n", winner);
+            return false;
         }
 
+        final Figure currentFigure = currentMoveController.currentMove(field);
+
+        if (currentFigure == null) {
+            System.out.println("No winner and no moves");
+            return false;
+        }
+
+        System.out.format("Please enter move point for: %s\n", currentFigure);
+        final Point point = askPoint();
+        try {
+            moveController.applyFigure(field, currentFigure, point);
+
+        } catch (InvalidPointException e) {
+            // e.printStackTrace();
+            invalidPointMassage("never exists");
+        } catch (PointAlreadyOccupiedException e) {
+            //e.printStackTrace();
+            invalidPointMassage("already occupied");
+        }
+
+
+        return true;
     }
 
-
-    public void move(final Game game) {
-
-
+    private void invalidPointMassage(String message) {
+        System.out.println();
+        System.out.format("Point is %s! TryAgain\n", message);
+        System.out.println();
     }
 
-    private void printeLine(final Field field, final int x) {
+    private Point askPoint() {
+        final int x = askCoordinate("X") - 1;
+        final int y = askCoordinate("Y") - 1;
+        return new Point(y, x);
+    }
+
+    private int askCoordinate(final String coordinateName) {
+        System.out.format("Please input %s coordinate: ", coordinateName);
+        final Scanner in = new Scanner(System.in);
+        try {
+            return in.nextInt();
+        } catch (final InputMismatchException e) {
+            System.out.format("\nO_o really?\n\n");
+            return askCoordinate(coordinateName);
+        }
+    }
+
+    private void printLine(final Field field, final int x) {
+
+        System.out.print(" ");
 
         for (int y = 0; y < field.getSize(); y += 1) {
 
@@ -40,14 +106,15 @@ public class ConsoleView {
                 e.printStackTrace();
                 throw new RuntimeException(e);
             }
+            if (y != 0) System.out.print(" | ");
             System.out.print(figure != null ? figure : " ");
-            if (y != field.getSize() - 1) System.out.print("|");
         }
+        System.out.print(" ");
     }
 
     private void printSeparator() {
         System.out.println();
-        System.out.println("------");
+        System.out.println("-----------");
     }
 
 
